1d0953e284a8946a7417b447aaf28e0dcb1e1bf8
Renan Henrique Da Fonte Costa <henriquebasshvf@gmail.com>
2019-06-04 05:59:06 -0300

    update same styles

--- src/components/CardsItem/index.js
+++ src/components/CardsItem/index.js
@@ -4,7 +4,7 @@ import { Text, View, Dimensions, TouchableOpacity } from 'react-native';
 const CardItem = ({ value }) => (
   <TouchableOpacity style={styles.container}>
     <View style={styles.content}>
-      <Text>{value}</Text>
+      <Text style={styles.text}>{value}</Text>
     </View>
   </TouchableOpacity>
 )
@@ -13,15 +13,18 @@ const CardItem = ({ value }) => (
 const styles = {
   container: {
     height: (Dimensions.get('window').width / 3) + 20,
-    width: (Dimensions.get('window').width / 3) - 8, 
+    width: (Dimensions.get('window').width / 3) - 16, 
     borderColor: 'red',
     borderWidth: 1,
-    margin: 4,
+    margin: 8,
   },
   content: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  text: {
+    fontSize: 20
   }
 }
 
